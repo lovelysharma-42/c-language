004d5eb5d20073adc6f4a0947affecaa6528b357
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-02 21:41:36 +0530

    Add files via upload

---
+++ using_puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	puts("I am Lovely Sharma.");
+	puts("I am a smart,honest,disciplined,easy-going and attentive person.");
+	puts("My hobbies include writing poems,gardening,cooking and listening to music");
+	return 0;
+}
