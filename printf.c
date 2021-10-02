5e239b4b2be8a976dccd3269e6e7271703615a42
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-02 20:51:58 +0530

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("I am Lovely Sharma\n");
+	printf("I am currently pursuing my B.Tech");
+	printf("\nMy stream is Information Technology");
+	printf("\nI am writing my code in C language");
+	return 0;
+}
+
