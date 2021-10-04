4d72f4d60f16b1b062e353623cbef915a45a50bc
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-04 19:49:52 +0530

    Add files via upload

---
+++ relational_operator.c
@@ -0,0 +1,26 @@
+//written by Lovely sharma
+//date : 4 oct,2021 & time : 5:56pm
+//program to see how relational operator works.
+// 0 output represnts false and 1 represents true.
+//By assigning the values of the three numbers.
+#include <stdio.h>
+
+int main(void) {
+      int a=10,b=9,c=5;
+      int p=a>b;
+      int q=b>=a;
+      int r=a<b;
+      int s=a==c;
+      int t=c!=a;
+      int u=b<=c;
+      
+      printf("The value of p is=%d\n",p);
+      printf("The value of q is =%d\n",q);
+      printf("The value of r is=%d\n",r);
+      printf("The value of s is=%d\n",s);
+      printf("The value os t is=%d\n",t);
+      printf("The value of u is=%d\n",u);
+      
+      
+        return 0;
+}
