24b5e33b6008514f8d13156caf0348e81fe1fd9b
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-05 18:05:23 +0530

    Add files via upload

---
+++ prefixOperator.c
@@ -0,0 +1,19 @@
+//BY LOVELY SHARMA
+//DATE : 5 OCT,2021
+//PROGRAM TO ILLUSTRATE THE USE OF UNARY PREFIX INCREMENT AND DECREMENT OPERATORS
+#include <stdio.h>
+
+int main(void) {
+        int count = 5;
+        //using prefix increment operator
+	printf("\n The value of count =%d",count);
+	printf("\n The value of count =%d",++count);
+	printf("\n The new value of count =%d",count);
+	//using prefix decrement operator
+	printf("\n The value of count =%d",count);
+	printf("\n The value of count =%d",--count);
+	printf("\n The new value of count =%d",count);
+	
+	return 0;
+}
+
