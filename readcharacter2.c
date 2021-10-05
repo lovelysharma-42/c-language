78eaae5f42bd032929b0901d0c2ece038e1ac16a
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-06 00:25:19 +0530

    lower to upper case
    taking character input from user in lowercase and then converting into uppercase without using any function.

---
+++ readcharacter2.c
@@ -0,0 +1,11 @@
+//by LOVELY SHARMA
+//program to take a character input in lower case converting it into upper case
+
+#include <stdio.h>
+int main(void) {
+        char la;
+        printf("\n enter any character in lower case: ");
+	scanf("%c",&la);
+	printf("\n The upper character is %c ",la-32);
+	return 0;
+	}
