265ed72b7f42f53a2be34ccd8da2eab15d21e681
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-05 20:08:47 +0530

    Add files via upload

---
+++ program_long_diff.c
@@ -0,0 +1,13 @@
+//by LOVELY SHARMA
+//program to subtract two long int.
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+        long int num1=5678919,num2,diff=0;
+        printf("\nEnter the number you want to subtract from num1:\n");
+	scanf("%ld",&num2);
+	diff= num1-num2;
+	printf("The difference is =%ld\n",diff);
+	return 0;
+	}
+
