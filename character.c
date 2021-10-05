3a59c807eeb7f2faf127c0c9870e70ee654c8abe
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-05 20:26:10 +0530

    character

---
+++ character.c
@@ -0,0 +1,14 @@
+//by LOVELY SHARMA
+//program to find ascii value of a character.
+
+#include <stdio.h>
+#define pi 3.14159
+int main(void) {
+        char ch;
+        printf("\n enter any character of your choice");
+	scanf("%c",&ch);
+	printf("\n The ASCII value of %c =%d",ch,ch);
+	printf("\n The  character is %c ",ch);
+	return 0;
+	}
+
