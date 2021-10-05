eeef41316d7d3b9956dbaec61d63b6b9c51793df
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-05 20:07:18 +0530

    Add files via upload

---
+++ bitwiseOperator.c
@@ -0,0 +1,17 @@
+//by lovely sharma
+//date: 5 oct,2021
+//a program to show how bitwise operator works
+#include <stdio.h>
+
+int main(void) {
+        int a=34,b=56;
+	printf("\n a & b = %d",a&b);
+	printf("\n a | b = %d",a|b);
+	printf("\n ~a = %d",~a);
+	printf("\n ~b = %d",~b);
+	printf("\n a ^ b = %d",a^b);
+	printf("\n a << 1 = %d",a<<1);
+	printf("\n b >> 1 = %d",b>>1);
+	return 0;
+}
+
