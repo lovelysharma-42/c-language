c8f7316be0c4d9732113ffa5368976362b376082
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-05 18:05:37 +0530

    Add files via upload

---
+++ postfixOperator.c
@@ -0,0 +1,19 @@
+//BY LOVELY SHARMA
+//DATE : 5 OCT,2021
+//PROGRAM TO ILLUSTRATE THE USE OF UNARY POSTFIX INCREMENT AND DECREMENT OPERATORS
+#include <stdio.h>
+
+int main(void) {
+        int count = 5;
+        //using postfix increment operator
+	printf("\n The value of count =%d",count);
+	printf("\n The value of count =%d",count++);
+	printf("\n The new value of count =%d",count);
+	//using postfix decrement operator
+	printf("\n The value of count =%d",count);
+	printf("\n The value of count =%d",count--);
+	printf("\n The new value of count =%d",count);
+	
+	return 0;
+}
+
