9ecd88bfdca3182f234c4d168e456d4a723e836c
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-06 18:11:53 +0530

    displaying size of datatypes

---
+++ displaying.c
@@ -0,0 +1,19 @@
+//by Lovely Sharma
+//date: oct 6,2021
+// program to show the size of every data type.
+#include <stdio.h>
+
+int main(void) {
+        
+	printf("\n The size of character =%ld",sizeof(char));
+	printf("\n The size of unsigned character =%ld",sizeof(unsigned char));
+	printf("\n The size of signed character =%ld",sizeof(signed char));
+	printf("\n The size of short integer =%ld",sizeof(short int));
+	printf("\n The size of unsigned integer =%ld",sizeof(unsigned int));
+	printf("\n The size of signed integer =%ld",sizeof(signed int));
+	printf("\n The size of integer = %ld",sizeof(int));
+	printf("\n The size of long integer = %ld",sizeof(long));
+	printf("\n The size of floating point number = %ld",sizeof(float));
+	printf("\n The size of double number = %ld",sizeof(double));
+        return 0;
+}
