f1c267bb24660ab24c334c17889af3d1bff04069
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-06 18:14:32 +0530

    type casting

---
+++ floatToint.c
@@ -0,0 +1,14 @@
+//written by Lovely sharma
+//date : 6 oct,2021 
+//program to convert float to corresponding integer.
+#include <stdio.h>
+int main(void) 
+{
+      float num;
+      int d1;
+      printf("\n enter any floating number ");
+      scanf("%f",&num);
+      d1=(float)num;
+      printf("\n The integer value of %f is = : %d",num,d1);
+      return 0;
+}
