222a9df72af0361e90a5e05d1bc897393e7a2e70
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-07 19:15:04 +0530

    Add files via upload
    program to assign value of integers and then displaying each value. Also print three numbers in a line seprated by comma.

---
+++ readint.c
@@ -0,0 +1,22 @@
+//by LOVELY SHARMA
+//program to assign value of integers and then displaying each value. Also print three numbers in a line seprated by comma. 
+#include <stdio.h>
+int main(void) {
+        int a=78,b=27,c=73,d=45,e=87,f=98,g=58,h=57,i=90,j=84;
+      	printf("\n value of a is %d",a);
+      	printf("\n value of b is %d",b);
+      	printf("\n value of c is %d",c);
+      	printf("\n value of d is %d",d);
+      	printf("\n value of e is %d",e);
+      	printf("\n value of f is %d",f);
+      	printf("\n value of g is %d",g);
+      	printf("\n value of h is %d",h);
+      	printf("\n value of i is %d",i);
+      	printf("\n value of j is %d",j);
+        printf("\n value of these three numbers in line are %d,%d,%d",a,b,c);
+      	printf("\n value of these three numbers in line are %d,%d,%d",d,e,f);
+      	printf("\n value of these three numbers in line are %d,%d,%d",g,h,i);
+      	printf("\n value of the last number in line is %d",j);
+	return 0;
+	}
+
