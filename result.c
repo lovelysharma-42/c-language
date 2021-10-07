5e6e17fd5390fa0a2a3b99d86e6276e32b914839
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-07 19:09:47 +0530

    storing result in int

---
+++ result.c
@@ -0,0 +1,16 @@
+//by LOVELY SHARMA
+//a program to take two floating point numbers. Add them and assign the result to an integer. 
+#include <stdio.h>
+int main(void) {
+        float num1,num2,num3;
+        int result;
+        printf("\n Enter any integer of your choice:");
+	scanf("%f",&num1);
+	printf("\n Enter any integer of your choice:");
+	scanf("%f",&num2);
+	result=(int)num1+num2;
+	printf("\n the result is %d",result);
+        return 0;
+	
+	}
+
