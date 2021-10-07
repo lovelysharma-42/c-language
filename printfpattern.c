40b76700181f344f7f1c6733b2722e549527c36d
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-07 21:42:27 +0530

    using printf for making pattern
    I
    AM
    LOVELY

---
+++ printfpattern.c
@@ -0,0 +1,26 @@
+//by Lovely Sharma
+//program using printf statement to print I in a specific pattern, then AM and finally LOVELY.
+#include <stdio.h>
+int main(void) {
+	printf("IIIIIIIII\n");
+	printf("    I    \n");
+	printf("    I    \n");
+	printf("    I    \n");
+	printf("IIIIIIIII\n");
+	printf("\n");
+	printf("\n");
+	printf("     A     M       M\n");
+	printf("    A A    M M   M M\n");
+	printf("   A A A   M   M   M\n");
+	printf("  A     A  M       M\n");
+	printf(" A       A M       M\n");
+	printf("\n");
+	printf("\n");
+	printf("L        OOO  V       V  EEEEEE  L      Y     Y\n");
+	printf("L       O   O  V     V   E       L       Y   Y\n");
+	printf("L       O   O   V   V    EEEE    L         Y\n");
+	printf("L       O   0    V V     E       L         Y \n");
+        printf("LLLLLL   OOO      V      EEEEEE  LLLLLLL   Y\n");
+	return 0;
+}
+
