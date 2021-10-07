c85e3fdc815020752c31d55fbe7063d300eb74d6
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-07 21:41:34 +0530

    creating bye pattern using printf only

---
+++ printingBYE.c
@@ -0,0 +1,13 @@
+//by Lovely Sharma
+//program using printf statement to print BYE in a specific pattern.
+#include <stdio.h>
+int main(void) {
+	printf("BBB     Y     Y     EEEE\n");
+	printf("B  B     Y   Y      E   \n");
+	printf("BBB        Y        EEEE\n");
+	printf("B  B       Y        E   \n");
+	printf("BBB        Y        EEEE\n");
+
+	return 0;
+}
+
