dafee7bc8f636024596a061c5d393ee8a7f3a191
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-08 20:36:58 +0530

    checking no. is even or odd

---
+++ evenOrodd.c
@@ -0,0 +1,14 @@
+//by LOVELY SHARMA
+//a program to determine whether enter number is even or odd .
+#include <stdio.h>
+int main(void) {
+        int num;
+        printf("\n Enter number of your choice:");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("\n Entered number is even.");
+	else
+	printf("\n Entered number is odd.");
+        return 0;
+	}
+
