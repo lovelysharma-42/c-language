700ea1831165a218177097d65057fffa1d504996
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-09 23:18:07 +0530

    using for loop displaying no.s 1 to 10
    using for loop displaying nos 1 to 10 vertically and horizontally

---
+++ num.c
@@ -0,0 +1,18 @@
+//by Lovely Sharma
+// print num from 1 to 10. First vertically then horizontally.
+#include<stdio.h>  
+int main()
+{  
+int i=1;   
+printf("printing numbers vertically");
+for(i=1;i<=10;i++)
+{        
+printf("%d \n",i); 
+}    
+for(i=1;i<=10;i++)
+{        
+printf("%d ",i); 
+}    
+  
+return 0;  
+}    
