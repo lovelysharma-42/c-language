ea5a49b67e855b46b02eb1b68fa4879ae51dcd47
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-09 23:35:31 +0530

    seeing how switch works

---
+++ useSwitch.c
@@ -0,0 +1,22 @@
+//by LOVELY SHARMA
+//desicion control statements practice
+//a program to demonstarte the use of switch.
+#include <stdio.h>
+int main(void) 
+{
+        int option =1;
+        switch(option)
+        {
+         case 1:
+         printf("\n In case 1");
+         break;
+ 
+         case 2:
+           printf("\n In case 2");
+         break;
+ 
+         default:
+           printf("\n In case default");
+         }
+        return 0;
+}        
