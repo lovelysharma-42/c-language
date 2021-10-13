1736ea73f40252e435aa4dc1cc26f8f5d81054b6
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-14 00:00:19 +0530

    Add files via upload

---
+++ asterisks.c
@@ -0,0 +1,20 @@
+//by LOVELY SHARMA
+//a program to print 20 asterisks in a horizontal line using while loop and another while loop for 20 vertical asterisks.
+#include <stdio.h>
+int main(void) 
+{
+        int i = 1;
+        while(i<=20)
+        {
+          printf("*");
+          i++;
+        }
+        int j=1;
+        while(j<=20)
+        {
+          printf("*\n");
+          j++;
+        }
+        
+        return 0;
+}        
