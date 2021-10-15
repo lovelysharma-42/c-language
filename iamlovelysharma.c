35627898a32d867451dffe979dd28793e0add319
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-15 23:53:32 +0530

    printing "i am lovely sharma" using do while loop

---
+++ iamlovelysharma.c
@@ -0,0 +1,14 @@
+//by LOVELY SHARMA
+//OCT 15,2021.
+//A PROGRAM TO PRINT "I AM LOVELY SHARMA " 100 TIMES.
+#include<stdio.h>
+int main()
+{
+  int i=1;
+  while (i<=100)
+  {
+  printf("\n I AM LOVELY SHARMA.");
+  i++;
+  }
+  return 0;
+}
