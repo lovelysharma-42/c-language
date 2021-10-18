787d8cfe0590137c5335d8c5aee1ae86d6314327
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-18 22:23:13 +0530

    printing 1000 to 1 numbers using for loop.

---
+++ forloop2.c
@@ -0,0 +1,15 @@
+//by LOVELY SHARMA
+//a program which prints 1000 to 1 numbers using for loop.
+#include <stdio.h>
+int main(void) 
+{
+        int i;
+       
+        for(i=1000;i>0;i--)
+        {
+         
+         printf("\n%d",i);
+             
+        }
+        return 0;
+}        
