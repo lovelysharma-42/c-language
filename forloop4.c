5c7a1af59affa2d3d965b90558a54b0963a8187c
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-18 22:25:30 +0530

    odd numbers from 1 to 30 using for loop

---
+++ forloop4.c
@@ -0,0 +1,15 @@
+//by LOVELY SHARMA
+//a program which prints odd numbers from 1 to 30 using for loop.
+#include <stdio.h>
+int main(void) 
+{
+        int i;
+       
+        for(i=1;i<30;i+=2)
+        {
+         
+         printf("\n%d",i);
+             
+        }
+        return 0;
+}        
