2a3e9192581c81e7ea31a66055d290c2ddbca8b1
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-18 22:21:48 +0530

    printing n numbers using for loop

---
+++ n_numbers.c
@@ -0,0 +1,17 @@
+//by LOVELY SHARMA
+//a program which prints the first n numbers using for loop.
+#include <stdio.h>
+int main(void) 
+{
+        int i,n;
+        
+        printf("\n\n enter any number: ");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+         
+         printf("\n%d",i);
+             
+        }
+        return 0;
+}        
