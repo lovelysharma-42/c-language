4dd0d6b8e5be19f23937d2e15a458c49fbce68d0
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-18 22:27:01 +0530

    printing pattern

---
+++ forloop6.c
@@ -0,0 +1,23 @@
+//by LOVELY SHARMA
+/*a program which prints  the following pattern using for loop.
+**********
+**********
+**********
+**********
+**********
+*/
+#include <stdio.h>
+int main(void) 
+{
+        int i,j;
+       
+        for(i=1;i<=5;i++)
+        {
+         
+         printf("\n");
+         for(j=1;j<=10;j++)
+          printf("*");
+             
+        }
+        return 0;
+}        
