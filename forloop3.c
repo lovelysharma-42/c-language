8532ff5ba87da73d4f7d8cf21c24ca27e6cea60a
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-18 22:24:39 +0530

    sum of first 10 natural numbers using for loop.

---
+++ forloop3.c
@@ -0,0 +1,15 @@
+//by LOVELY SHARMA
+//a program which print sum of first 10 natural numbers using for loop.
+#include <stdio.h>
+int main(void) 
+{
+        int i,sum;
+       
+        for(i=1,sum=0;i<10;i++)
+        {
+         sum+=i;
+         printf("\n%d",sum);
+             
+        }
+        return 0;
+}        
