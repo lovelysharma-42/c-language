03a38bdc3b4e6e618284ab29ce86be5255c50246
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-25 19:00:01 +0530

    multiplication table of 10 in reversed order

---
+++ for_mul10.c
@@ -0,0 +1,15 @@
+//by LOVELY SHARMA
+//a program to the multiplication table of 10 in reversed order. 
+
+#include <stdio.h>
+int main(void) 
+{
+        int i;
+	printf(" Multiplication table of 10");
+	printf(" \n ********************************************");
+	for(i=20;i>=1;i--)
+	printf(" \n 10 * %d=%d",i,(10*i));
+	
+	return 0;
+}
+
