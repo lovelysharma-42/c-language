6a8aa7356cca5e5224b7b6196d781df5f0510005
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-10-28 23:48:12 +0530

    finding reverse of a number

---
+++ reverse_num.c
@@ -0,0 +1,14 @@
+//by LOVELY SHARMA
+//a program to find reverse of a number.
+#include <stdio.h>
+int main(void) 
+{
+        int num,temp = 0;
+        while(i<=10)
+        {
+          sum = sum+i;
+          i++;
+        }
+        printf("\n the sum is : %d",sum);
+        return 0;
+}        
