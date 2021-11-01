0a26c7ec4632d175d04fba5acca8f984dcd4c144
lovelysharma-42 <86016294+lovelysharma-42@users.noreply.github.com>
2021-11-01 23:44:59 +0530

    using continue keyword

--- continue2.c
+++ continue2.c
@@ -1,11 +1,16 @@
+//by  LOVELY SHARMA
+//PROGRAM FOR EXAMPLE OF - C continue statement continues for only inner loop, but not for outer loop.
 #include<stdio.h>  
 int main(){  
-int i=1; 
-for(i=1;i<=10;i++){      
-if(i!=5){    
-continue;    
+int i=1,j=1;//initializing a local variable    
+for(i=1;i<=3;i++){      
+for(j=1;j<=3;j++){    
+if(i==2 && j==2){    
+continue;//will continue loop of j only
+//2 2 is not printed on the console because inner loop is continued at i==2 and j==2    
 }    
-printf("%d \n",i);    
+printf("%d %d\n",i,j);    
 }    
+}  
 return 0;  
-}    
+}
